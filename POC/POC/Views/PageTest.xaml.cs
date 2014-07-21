7bf3305f22eb159318627ca9c01c5c791bf2e6ea
Sebastien MOULIGNEAUX <s.mouligneaux@expert-ms.fr>
2014-07-21 16:37:48 +0200

    Suppression parties de codes inutiles (ancien projet POC)

--- POC/POC/Views/PageTest.xaml.cs
+++ POC/POC/Views/PageTest.xaml.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using POC.Data;
 using POC.DataModels;
 using POC.Models;
-using POC.ViewModels;
+
 using SQLite.Net;
 using Xamarin.Forms;
 
@@ -42,25 +42,6 @@ namespace POC.Views
             this.Content = lv;
         }
 
-        //private async void LoadVisuel()
-        //{
-        //    ListView lv = new ListView();
-
-        //    lv.ItemsSource = await ServiceProxy.Instance.Service.GetVelib();
-
-        //    lv.ItemTemplate = new DataTemplate(() =>
-        //    {
-        //        TextCell tc = new TextCell();
-        //        tc.SetBinding(TextCell.TextProperty, "name");
-        //        return tc;
-        //    });
-
-        //    //Enregistrement dans BDD Sqlite
-
-
-
-        //    this.Content = lv;
-        //}
 
 
 
